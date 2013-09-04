839264d164d41a7bd4cfbff3fa07131e16911cc0
Chanon <Chanon@192.168.118.48>
2013-09-04 14:26:42 +0700

    test ui with alloy v2

--- index.php
+++ index.php
@@ -1,24 +1,63 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="./alloy/build/aui-css/css/bootstrap.css">
+	<script src="./alloy/build/aui/aui.js"></script>
+</head>
 <body>
 
 <div id="container" style="width:100%">
 
-<div id="header" style="background-color:#FF0000;">
+<div id="header" style="background-color:#CEECF5;width:100%;height:200px;">
 <h1 style="margin-bottom:0;">Main Title of Web Page</h1></div>
 
 
 
-<div id="content" style="background-color:#EEEEEE;height:200px;width:100%;float:left;">
+<div id="content" style="background-color:#E6E6E6;height:500px;width:100%;float:left;">
 Content goes here
-<div id="menu" style="background-color:#FFD700;height:125px;width:400px;float:center;margin:auto;text-align:center;margin-top:25px;">test
+<div id="menu" style="background-color:#F5A9F2;height:80%;width:50%;float:center;margin:auto;margin-top:25px;">
+    
+    
+    <form id="myForm" style="padding-top:15%;padding-left:30%;">
+    <div class="control-group">
+        <div class="controls" >       
+          <label class="control-label" for="name">User Name: <input name="name" id="name" class="field-required" type="text" style="margin-left:22px;"></label>  
+        </div>
+    </div>
+        
+    <div class="control-group" >
+        <div class="controls" >    
+         <label class="control-label" for="pass">PassWord: <input name="pass" id="pass" class="field-required" type="text" style="margin-left:25px;"></label> 
+        </div>
+    </div>
+        
+        <input class="btn btn-info" type="submit" value="Submit" style="margin-left:20%;">
+        <input class="btn btn-primary" type="reset" value="Reset">
+    </form>  
+    
+    
 </div>
 </div>
 
 <div id="footer" style="background-color:#FFA500;clear:both;text-align:center;">
-Copyright © W3Schools.com</div>
+Copyright © Chanon</div>
 
 </div>
  
+<script>
+   YUI().use(
+  'aui-form-validator',
+  function(Y) {
+    new Y.FormValidator(
+      {
+        boundingBox: '#myForm'
+      }
+    );
+  }
+);
+</script>
+    
 </body>
 </html>
